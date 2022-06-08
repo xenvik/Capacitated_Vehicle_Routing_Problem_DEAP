d4237aa69e667227c85984e7fc06f3b292f5987f
xenvik <55597813+xenvik@users.noreply.github.com>
2022-06-08 15:05:09 +0700

    Add files via upload

---
+++ test_algo_methods.py
@@ -0,0 +1,21 @@
+import unittest
+import math
+from text_to_json import calculate_distance
+from deap_model import input_instance
+
+
+class TestAlgoMethods(unittest.TestCase):
+
+
+    def test_crossover(self):
+        # To test if given crossover producing correct crossover behaviour
+        pass
+
+    def test_mutation(self):
+        # To test if given mutation producing correct behaviour
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
